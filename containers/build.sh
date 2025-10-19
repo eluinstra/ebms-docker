271d846d15a9d133ffd793d7ac0c299286fd0a42
eluinstra <edwin.luinstra@soprasteria.nl>
2025-10-19 19:07:49 +0200

    updated to ebms-admin version 2.20.3

--- containers/build.sh
+++ containers/build.sh
@@ -6,6 +6,7 @@ export BASE_DIR=`dirname $(realpath $0)`
 cd $BASE_DIR/ebms-adapter-bin/
 docker build \
   --build-arg EBMS_VERSION=$EBMS_VERSION \
+  -t $REPO/ebms-adapter-bin:$EBMS_VERSION \
   -t $REPO/ebms-adapter-bin:$EBMS_VERSION-$ARCH \
   -t $REPO/ebms-adapter-bin:$EBMS_MAJOR_VERSION-$ARCH \
   -t $REPO/ebms-adapter-bin:latest-$ARCH \
@@ -15,6 +16,7 @@ cd $BASE_DIR/ebms-adapter-pg/
 docker build \
   --build-arg EBMS_VERSION=${EBMS_VERSION} \
   --build-arg POSTGRES_DRIVER=postgresql-$POSTGRES_VERSION.jar \
+  -t $REPO/ebms-adapter-pg:$EBMS_VERSION \
   -t $REPO/ebms-adapter-pg:$EBMS_VERSION-$ARCH \
   -t $REPO/ebms-adapter-pg:$EBMS_MAJOR_VERSION-$ARCH \
   -t $REPO/ebms-adapter-pg:latest-$ARCH \
@@ -23,6 +25,7 @@ docker build \
 cd $BASE_DIR/activemq/
 docker build \
   --build-arg ACTIVEMQ_VERSION=$ACTIVEMQ_VERSION \
+  -t $REPO/activemq:$ACTIVEMQ_VERSION \
   -t $REPO/activemq:$ACTIVEMQ_VERSION-$ARCH \
   -t $REPO/activemq:latest-$ARCH \
   .
--- containers/env.sh
+++ containers/env.sh
@@ -2,7 +2,7 @@
 
 export ARCH=`dpkg --print-architecture`
 export EBMS_MAJOR_VERSION=2.20
-export EBMS_VERSION=$EBMS_MAJOR_VERSION.2
-export ACTIVEMQ_VERSION=6.1.3
-export POSTGRES_VERSION=42.7.5
+export EBMS_VERSION=$EBMS_MAJOR_VERSION.3
+export ACTIVEMQ_VERSION=6.1.7
+export POSTGRES_VERSION=42.7.7
 export REPO=eluinstra
