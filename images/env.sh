cfb1d7248ed2ef7c4837cde130b388f8ed3fc60f
eluinstra <edwin.luinstra@soprasteria.nl>
2026-05-30 15:15:33 +0000

    updated to ebms-admin-2.20.6

--- images/env.sh
+++ images/env.sh
@@ -2,5 +2,5 @@
 
 export ARCH=`dpkg --print-architecture`
 export EBMS_MAJOR_VERSION=2.20
-export EBMS_VERSION=$EBMS_MAJOR_VERSION.5
+export EBMS_VERSION=$EBMS_MAJOR_VERSION.6
 export REPO=eluinstra
