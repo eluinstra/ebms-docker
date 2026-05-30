85c8d9ed7d2234d12d34da33424391740d6ccbd8
eluinstra <edwin.luinstra@soprasteria.nl>
2026-05-30 15:07:29 +0000

    updated to ebms-admin-2.20.5

--- images/env.sh
+++ images/env.sh
@@ -2,6 +2,5 @@
 
 export ARCH=`dpkg --print-architecture`
 export EBMS_MAJOR_VERSION=2.20
-export EBMS_VERSION=$EBMS_MAJOR_VERSION.4
-export POSTGRES_VERSION=42.7.7
+export EBMS_VERSION=$EBMS_MAJOR_VERSION.5
 export REPO=eluinstra
